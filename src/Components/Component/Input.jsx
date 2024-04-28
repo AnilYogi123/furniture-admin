7dde62dfb8a33a0bf02214ce64e3f93775415866
Anil Yogi <np03cs4s220204@heraldcollege.edu.np>
2024-04-28 14:43:09 +0545

    space hatako

--- src/Components/Component/Input.jsx
+++ src/Components/Component/Input.jsx
@@ -1,12 +1,19 @@
-
 const Input = ({ label, type ,register,registerName}) => {
+
+    const onChange = (e) => {
+        const newValue = e.target.value.replace(/\s+/g, '');
+        e.target.value = newValue; // Set the processed value back to the input
+        return register.onChange(e); // Ensure we pass the event back to react-hook-form
+    };
+
     return (
         <div className="w-full mb-3">
             <label className='text-sm font-normal text-[#596579]'>{label}</label>
             <input
                 type={type}
                 required
-                {...register(`${registerName}`)}
+                onChange={onChange}
+                {...register(registerName, { onChange })}
                 className="mt-1 px-3 py-1 border rounded-lg text-sm font-normal text-[#596579] w-full"
             />
         </div>
